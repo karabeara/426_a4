bac4b6c770990c8fa96140e998ec697d06d4f7ed
Kara <karab@princeton.edu>
2017-04-27 22:27:56 -0400

    Changed student name

--- js/student.js
+++ js/student.js
@@ -1,8 +1,8 @@
 var Student = {
     // please fill in your name and NetID
     // your NetID is the part of your email before @princeton.edu
-    'name'  : 'Student Name',
-    'netID' : 'NetID',
+    'name'  : 'Kara Bressler and Khaled Tinubu',
+    'netID' : 'karab, ktinubu',
 };
 
 Student.updateHTML = function( ) {
